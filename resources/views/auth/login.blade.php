84e089634d3a81dee1086a32b5c14b1dcdc120d9
salahulbain <salahul.bain@gmail.com>
2022-07-16 22:13:48 +0700

    modify login ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,82 @@
 <x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+    <main class="mt-0 transition-all duration-200 ease-soft-in-out">
+        <section>
+            <div class="relative flex items-center p-0 overflow-hidden bg-center bg-cover min-h-75-screen">
+                <div class="container z-10">
+                    <div class="flex flex-wrap mt-0 -mx-3">
+                        <div
+                            class="flex flex-col w-full max-w-full px-3 mx-auto md:flex-0 shrink-0 md:w-6/12 lg:w-5/12 xl:w-4/12">
+                            <div
+                                class="relative flex flex-col min-w-0 mt-20 break-words bg-transparent border-0 shadow-none rounded-2xl bg-clip-border">
+                                <div class="p-6 pb-0 mb-0 bg-transparent border-b-0 rounded-t-2xl">
+                                    <a href="/">
+                                        <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                                    </a>
+                                    <h3 class="relative z-10 font-bold text-transparent bg-gradient-cyan bg-clip-text">
+                                        Welcome back</h3>
+                                    <p class="mb-0">Enter your email and password to sign in</p>
+                                </div>
+                                <div class="flex-auto p-6">
+                                    <!-- Session Status -->
+                                    <x-auth-session-status class="mb-4" :status="session('status')" />
+                                    <!-- Validation Errors -->
+                                    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <label class="mb-2 ml-1 font-bold text-size-xs text-slate-700">Email</label>
+                                        <div class="mb-4">
+                                            <input type="email"
+                                                class="focus:shadow-soft-primary-outline text-size-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow"
+                                                placeholder="Email" aria-label="Email" aria-describedby="email-addon"
+                                                name="email" :value="old('email')" required autofocus />
+                                        </div>
+                                        <label class="mb-2 ml-1 font-bold text-size-xs text-slate-700">Password</label>
+                                        <div class="mb-4">
+                                            <input type="password"
+                                                class="focus:shadow-soft-primary-outline text-size-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow"
+                                                placeholder="Password" aria-label="Password"
+                                                aria-describedby="password-addon" name="password" required
+                                                autocomplete="current-password" />
+                                        </div>
+                                        <div class="min-h-6 mb-0.5 block pl-12">
+                                            <input id="rememberMe"
+                                                class="mt-0.54 rounded-10 duration-250 ease-soft-in-out after:rounded-circle after:shadow-soft-2xl after:duration-250 checked:after:translate-x-5.25 h-5-em relative float-left -ml-12 w-10 cursor-pointer appearance-none border border-solid border-gray-200 bg-slate-800/10 bg-none bg-contain bg-left bg-no-repeat align-top transition-all after:absolute after:top-px after:h-4 after:w-4 after:translate-x-px after:bg-white after:content-[''] checked:border-slate-800/95 checked:bg-slate-800/95 checked:bg-none checked:bg-right"
+                                                type="checkbox" checked="" name="remember" />
+                                            <label
+                                                class="mb-2 ml-1 font-normal cursor-pointer select-none text-size-sm text-slate-700"
+                                                for="rememberMe">Remember me</label>
+                                        </div>
+                                        <div class="text-center">
+                                            <button type="submit"
+                                                class="inline-block w-full px-6 py-3 mt-6 mb-0 font-bold text-center text-white uppercase align-middle transition-all bg-transparent border-0 rounded-lg cursor-pointer shadow-soft-md bg-x-25 bg-150 leading-pro text-size-xs ease-soft-in tracking-tight-soft bg-gradient-cyan hover:scale-102 hover:shadow-soft-xs active:opacity-85">Sign
+                                                in</button>
+                                        </div>
+                                    </form>
+                                </div>
+                                @if (Route::has('password.request'))
+                                <div
+                                    class="p-6 px-1 pt-0 text-center bg-transparent border-t-0 border-t-solid rounded-b-2xl lg:px-2">
+                                    <p class="mx-auto mb-6 leading-normal text-size-sm">
+                                        Forgot your password?
+                                        <a href="{{ route('password.request') }}""
+                                            class=" relative z-10 font-semibold text-transparent bg-gradient-cyan
+                                            bg-clip-text">Reset password
+                                        </a>
+                                    </p>
+                                </div>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="w-full max-w-full px-3 lg:flex-0 shrink-0 md:w-6/12">
+                            <div
+                                class="absolute top-0 hidden w-3/5 h-full -mr-32 overflow-hidden -skew-x-10 -right-40 rounded-bl-xl md:block">
+                                <div class="absolute inset-x-0 top-0 z-0 h-full -ml-16 bg-cover skew-x-10"
+                                    style="background-image: url('{{ asset('frontend/img/curved6.jpg') }}')"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
+        </section>
+    </main>
 </x-guest-layout>
