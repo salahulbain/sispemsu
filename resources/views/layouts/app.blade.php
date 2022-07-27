1f1369371f65664c41e156b836dea31d3481dbff
salahulbain <salahul.bain@gmail.com>
2022-07-27 12:07:27 +0700

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,13 +20,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     {{-- favicon --}}
-    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('frontend/favicon/apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('frontend/favicon/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('frontend/favicon/favicon-16x16.png') }}">
-    <link rel="manifest" href="{{ asset('frontend/favicon/site.webmanifest') }}">
-    <link rel="mask-icon" href="{{ asset('frontend/favicon/safari-pinned-tab.svg') }}" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
+    <x-favicon />
     <title>
         {{ config('app.name', 'SISPEMSU') }} -- {{ $title }}
     </title>
